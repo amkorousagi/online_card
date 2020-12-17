905ee07796168f578495b032c240eac9287896fe
psc <hasmi5452@gmail.com>
2020-12-17 22:30:11 +0900

    fd

--- join.js
+++ join.js
@@ -6,14 +6,35 @@ class Join{
             try {
                 await connection.beginTransaction(); // START TRANSACTION
                
-                const [rows] = await connection.execute(
-                    
+                const result0 = await connection.execute(
                     `INSERT INTO customer (id, pw, nickname) VALUES ('${id}', '${pw}', '${nickname}')`
-                    );
+                );
+
+                const result = await connection.execute(
+                    `INSERT INTO card (name, address, phone_number, face_photo, description) VALUES (?, ?, ?, ?, ?)`,
+                    ["name", "address", "phone_number", "url", "description"]
+                );
+                if(result[0].length == 0) {
+                    throw new Error("wrong query as inserting card")
+                }
+
+                const result2 = await connection.execute(
+                    `
+                    update customer
+                    set card_id = ?
+                    where id = ?
+                    `,
+                    [result[0].insertId, id]
+                );
+                if(result2[0].length == 0) {
+                    throw new Error("wrong query as updating customer")
+                }
+
+
                 await connection.commit(); // COMMIT
                 connection.release();
-                console.log(rows);
-                res.send(rows);
+                console.log(result0[0]);
+                res.send(result0[0]);
             } catch(err) {
                 await connection.rollback(); // ROLLBACK
                 connection.release();
